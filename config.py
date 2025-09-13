bd07c9bf2cc06af06a20d3bced81237e8464ae2f
D4rkZero <aspertix14@gmail.com>
2025-09-14 00:16:00 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Discord Bot
+DISCORD_TOKEN = "MTQwNjk0NTc4MTMxODk0Mjg5Mg.GvkFp8.0ZK8cZYcTSVnFmS4tWAdvya2xgEtJfYe9vpQvU"
+
+# BlockCypher (for LTC deposits)
+BLOCKCYPHER_TOKEN = "6bd854f80dba45b68c396c194e9f6fa9"
+XPUB = "xpub661MyMwKB68aRPSx3nLUJzVsRaisqw1sXYBCLBhiKadEeUCmkVCHxeoUstGTsWMeXMz6y3BEcXrsvKxkJPJzZeQeYXZLjHP7sHM1ZxpjM3Z"
+
+# Database
+DB_FILE = "casino.db"
+
+# Discord Webhooks
+DEPOSIT_WEBHOOK = "https://discord.com/api/webhooks/1408128076147327078/JtF0Df__UT3JYJluuycNRITkuMp9hHNqLuZnJybuYdqiAQ2ZsVivZKFUBJmV4GjjDE4l"
+WITHDRAW_WEBHOOK = "https://discord.com/api/webhooks/1408128079188201604/e3tTYnbfGXvhO36ODFqRczAKfO0u-9cwhy_OcjzTqO-DGW94PJ3pWj5fg3s54proTfz4"
